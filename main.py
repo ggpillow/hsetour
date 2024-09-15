8e12ed8cf7d31fd99646f92fc2801d3c29d34c6e
ggpillow <ketnut3336@gmail.com>
2024-09-15 20:09:31 +1100

    added kids

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ msc_price = 80_000
 ekb_price = 40_000
 
 city = input("Введите город: ")
-count_people = int(input("Введите количество туристов: "))
+count_adult_people = int(input("Введите количество взрослых туристов: "))
+count_kids_people = int(input("Введите количество детей: "))
 
 if city == "Питер":
-    total = spb_price * count_people
+    total = spb_price * count_adult_people + spb_price * count_kids_people / 2
 elif city == "Москва":
-    total = msc_price * count_people
+    total = msc_price * count_adult_people + msc_price * count_kids_people / 2
 elif city == "Екатеринбург":
-    total = ekb_price * count_people
+    total = ekb_price * count_adult_people + ekb_price * count_kids_people / 2
 else:
     print("Такого города нет")
     exit()
