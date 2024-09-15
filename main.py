1637829aa97df83ba78f095b08db1023bbc24238
ggpillow <ketnut3336@gmail.com>
2024-09-15 19:59:19 +1100

    refresh total

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+from itertools import count
+
 spb_price = 50_000
 msc_price = 80_000
 ekb_price = 40_000
+
+city = input("Введите город: ")
+count_people = int(input("Введите количество туристов: "))
+
+if city == "Питер":
+    total = spb_price * count_people
+elif city == "Москва":
+    total = msc_price * count_people
+elif city == "Екатеринбург":
+    total = ekb_price * count_people
+else:
+    print("Такого города нет")
+    exit()
+
+print("Вам нужно денег: " + str(total))
